daa446a856043e5897a7c98d80762beaf5351519
dnovysh <dnovysh@gmail.com>
2024-10-08 23:48:34 +0300

    refactor: base project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # t1-java-10
 Открытая школа разработчиков на JAVA 10 поток
+
+### Запуск Postgres контейнера
+docker run --name t1_java_demo -e POSTGRES_DB=t1_demo -e POSTGRES_PASSWORD=0000 -p 5432:5432 -d postgres:16.4-bookworm
--- src/main/java/ru/t1/java/demo/service/LegacyClientService.java
+++ src/main/java/ru/t1/java/demo/service/LegacyClientService.java
@@ -1,51 +1,59 @@
 package ru.t1.java.demo.service;
 
 import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.stereotype.Service;
 import ru.t1.java.demo.dto.ClientDto;
+import ru.t1.java.demo.exception.ClientException;
 import ru.t1.java.demo.model.Client;
 import ru.t1.java.demo.repository.ClientRepository;
 import ru.t1.java.demo.util.ClientMapper;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 @Slf4j
 public class LegacyClientService {
-    private final ClientRepository repository;
-    private final Map<Long, Client> cache;
 
-    public LegacyClientService(ClientRepository repository) {
-        this.repository = repository;
-        this.cache = new HashMap<>();
+  private final ClientRepository repository;
+  private final Map<Long, Client> cache;
+
+  public LegacyClientService(ClientRepository repository) {
+    this.repository = repository;
+    this.cache = new HashMap<>();
+  }
+
+  @PostConstruct
+  void init() {
+    val client = getClient(1L);
+    if (client != null) {
+      log.debug("Init cache - client with id {} added to cache", client.getId());
+    } else {
+      log.debug("Init cache - no clients added to cache");
+    }
+  }
+
+  public ClientDto getClient(Long id) {
+    log.debug("Call method getClient with id {}", id);
+    ClientDto clientDto = null;
+
+    if (cache.containsKey(id)) {
+      return ClientMapper.toDto(cache.get(id));
     }
 
-    @PostConstruct
-    void init() {
-        getClient(1L);
-    }
-
-    public ClientDto getClient(Long id) {
-        log.debug("Call method getClient with id {}", id);
-        ClientDto clientDto = null;
-
-        if (cache.containsKey(id)) {
-            return ClientMapper.toDto(cache.get(id));
-        }
-
-        try {
-            Client entity = repository.findById(id).get();
-            clientDto = ClientMapper.toDto(entity);
-            cache.put(id, entity);
-        } catch (Exception e) {
-            log.error("Error: ", e);
+    try {
+      Client entity = repository.findById(id)
+          .orElseThrow(() -> new ClientException("Not Found Client with id %d".formatted(id)));
+      clientDto = ClientMapper.toDto(entity);
+      cache.put(id, entity);
+    } catch (Exception e) {
+      log.error("Error: {}", e.getMessage());
 //            throw new ClientException();
-        }
+    }
 
 //        log.debug("Client info: {}", clientDto.toString());
-        return clientDto;
-    }
+    return clientDto;
+  }
 
 }
